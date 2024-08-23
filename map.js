4345dae93ba4ed5e4b9d57ecdf42a794dcbaf59b
Courtney Simonse <courtney@artemismaps.com>
2024-08-23 15:41:16 -0400

    add terrain

--- map.js
+++ map.js
@@ -22,6 +22,45 @@ const portsFeatures = await d3.csv('https://docs.google.com/spreadsheets/d/1n-2K
 
 map.on('load', () => {
 
+    map.addSource('elevation', {
+        "type": "raster-dem",
+        "tiles": ["https://tiles.stadiamaps.com/data/terrarium/{z}/{x}/{y}.png"],
+        "minzoom": 0,
+        "maxzoom": 14,
+        "encoding": "terrarium"
+    }).addLayer({
+        "id": "hillshade",
+        "type": "hillshade",
+        "source": "elevation",
+        "maxzoom": 14,
+        "paint": {
+            "hillshade-exaggeration": 0.2,
+            "hillshade-shadow-color": "#5b4128",
+            "hillshade-accent-color": "#5b4128",
+            "hillshade-highlight-color": "#e1e1e1"
+        }
+    })
+
+// Terrain attribution
+//     * ArcticDEM terrain data DEM(s) were created from DigitalGlobe, Inc., imagery and
+//   funded under National Science Foundation awards 1043681, 1559691, and 1542736;
+// * Australia terrain data © Commonwealth of Australia (Geoscience Australia) 2017;
+// * Austria terrain data © offene Daten Österreichs – Digitales Geländemodell (DGM)
+//   Österreich;
+// * Canada terrain data contains information licensed under the Open Government
+//   Licence – Canada;
+// * Europe terrain data produced using Copernicus data and information funded by the
+//   European Union - EU-DEM layers;
+// * Global ETOPO1 terrain data U.S. National Oceanic and Atmospheric Administration
+// * Mexico terrain data source: INEGI, Continental relief, 2016;
+// * New Zealand terrain data Copyright 2011 Crown copyright (c) Land Information New
+//   Zealand and the New Zealand Government (All rights reserved);
+// * Norway terrain data © Kartverket;
+// * United Kingdom terrain data © Environment Agency copyright and/or database right
+//   2015. All rights reserved;
+// * United States 3DEP (formerly NED) and global GMTED2010 and SRTM terrain data
+//   courtesy of the U.S. Geological Survey.
+
     map.addSource('trips', {
         'type': 'geojson',
         'data': tripsJson
